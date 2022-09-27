6338c58f194dbc78a0dc640cdf7229a8a021ad2e
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-09-27 14:49:58 +0300

    Completed Assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,3 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
-    </div>
-  );
-}
-
-export default ProjectItem;
+function ProjectItem({ name, about, technologies }) {gir
